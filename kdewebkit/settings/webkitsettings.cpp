2340ff03d7070d4f688298da05d2032967d0a239
avbidder@fortytwo.ch <avbidder@fortytwo.ch>
2009-07-15 14:23:02 +0200

    import upstream
    --HG--
    branch : upstream

--- kdewebkit/settings/webkitsettings.cpp
+++ kdewebkit/settings/webkitsettings.cpp
@@ -655,6 +655,7 @@ void WebKitSettings::init( KConfig * config, bool reset )
 #if QT_VERSION >= 0x040500
   QWebSettings::globalSettings()->setAttribute(QWebSettings::ZoomTextOnly, zoomTextOnly());
 #endif
+  QWebSettings::globalSettings()->setAttribute(QWebSettings::DeveloperExtrasEnabled, isJavaScriptDebugEnabled());
   QWebSettings::globalSettings()->setFontFamily(QWebSettings::StandardFont, stdFontName());
   QWebSettings::globalSettings()->setFontFamily(QWebSettings::FixedFont, fixedFontName());
   QWebSettings::globalSettings()->setFontFamily(QWebSettings::SerifFont, serifFontName());
@@ -775,7 +776,7 @@ void WebKitSettings::addAdFilter( const QString &url )
     // ### refactor somewhat
     if (url.length()>2 && url[0]=='/' && url[url.length()-1] == '/')
     {
-        QString inside = url.mid(1, url.length()-2);
+        const QString inside = url.mid(1, url.length()-2);
         rx.setPattern(inside);
     }
     else
@@ -787,7 +788,7 @@ void WebKitSettings::addAdFilter( const QString &url )
     if (rx.isValid())
     {
         int last=config.readEntry("Count", 0);
-        QString key = "Filter-" + QString::number(last);
+        const QString key = "Filter-" + QString::number(last);
         config.writeEntry(key, url);
         config.writeEntry("Count",last+1);
         config.sync();
--- part/webview.cpp
+++ part/webview.cpp
@@ -162,6 +162,10 @@ void WebView::partActionPopupMenu(KParts::BrowserExtension::ActionGroupMap &part
         partActions.append(action);
     }
 
+    if (settings()->testAttribute(QWebSettings::DeveloperExtrasEnabled)) {
+        partActions.append(pageAction(QWebPage::InspectElement));
+    }
+
     partGroupMap.insert("partactions", partActions);
 }
 
@@ -250,7 +254,7 @@ void WebView::WebViewPrivate::addSearchActions(QList<QAction *>& selectActions)
 
     QString name;
     if (KUriFilter::self()->filterUri(data, list)) {
-        QString iconPath = KStandardDirs::locate("cache", KMimeType::favIconForUrl(data.uri()) + ".png");
+        const QString iconPath = KStandardDirs::locate("cache", KMimeType::favIconForUrl(data.uri()) + ".png");
         if (iconPath.isEmpty())
             icon = KIcon("edit-find");
         else
@@ -288,7 +292,7 @@ void WebView::WebViewPrivate::addSearchActions(QList<QAction *>& selectActions)
             data.setData(searchProviderPrefix + "some keyword");
 
             if (KUriFilter::self()->filterUri(data, list)) {
-                QString iconPath = KStandardDirs::locate("cache", KMimeType::favIconForUrl(data.uri()) + ".png");
+                const QString iconPath = KStandardDirs::locate("cache", KMimeType::favIconForUrl(data.uri()) + ".png");
                 if (iconPath.isEmpty())
                     icon = KIcon("edit-find");
                 else
@@ -335,3 +339,4 @@ void WebView::openSelection()
 
     emit d->part->browserExtension()->openUrlRequest(d->selectedTextAsOneLine(), KParts::OpenUrlArguments(), browserArgs);
 }
+
